2ff48eb4290ba3a818a169374cdab5fea9007f1e
Benedikt Diemer <benedikt.diemer@cfa.harvard.edu>
2015-10-26 20:37:34 -0400

    Updated version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name = 'colossus',
-	version = '0.9.4',
+	version = '0.9.5',
 	description = ' Cosmology, halo and large-scale structure tools',
 	url = 'https://bitbucket.org/bdiemer/colossus',
 	author = 'Benedikt Diemer',
