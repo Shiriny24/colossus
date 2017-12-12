20ad2152a343bf06898bb9590f91059ddbecc2f7
Benedikt Diemer <benedikt.diemer@cfa.harvard.edu>
2017-12-12 13:48:38 -0500

    Increased version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.rst', encoding = 'utf-8') as f:
 	long_description = f.read()
 	
 setup(name = 'colossus',
-	version = '1.1.1',
+	version = '1.2.0',
 	description = 'Cosmology, halo, and large-scale structure tools',
 	long_description = long_description,
 	url = 'https://bitbucket.org/bdiemer/colossus',
